805b393fcfe3f10a52049f746c2641e43fca19d8
Gregoire Lefevre <lefevregregoire@proton.me>
2026-05-29 14:15:11 +0200

    Remove all comments from ProduitTest.cs

--- AtelierStock/AtelierStockTest/ProduitTest.cs
+++ AtelierStock/AtelierStockTest/ProduitTest.cs
@@ -6,14 +6,11 @@ namespace AtelierStock
         [TestMethod]
         public void Initialiser_ProduitQuelconque()
         {
-            // Arrange
             var prixAchat = 100.0m;
             var marge = 0.18m;
 
-            // Act
             var p = new Produit("REF", "UnNom", prixAchat, marge);
 
-            // Assert
             Assert.AreEqual("REF"  , p.Reference);
             Assert.AreEqual("UnNom", p.Libelle);
             Assert.AreEqual(100.0m , p.PrixAchat);
@@ -24,11 +21,8 @@ namespace AtelierStock
         [TestMethod]
         public void Initialiser_ProduitMarge0()
         {
-            // Arrange
-            // Act
             var p = new Produit("REF", "UnNom", 100, 0);
 
-            // Assert
             Assert.AreEqual("REF", p.Reference);
             Assert.AreEqual("UnNom", p.Libelle);
             Assert.AreEqual(100.0m, p.PrixAchat);
@@ -50,27 +44,22 @@ namespace AtelierStock
         {
             var p = new Produit("REF", "Nom", 10, 0);
 
-            // initial
             Assert.AreEqual(0, p.Stocks);
             Assert.IsTrue(p.EstEnRupture);
 
-            // rentrer du stock
             p.Rentrer(5);
             Assert.AreEqual(5, p.Stocks);
             Assert.IsFalse(p.EstEnRupture);
 
-            // sortir moins que le stock
             var sorti = p.Sortir(3);
             Assert.AreEqual(3, sorti);
             Assert.AreEqual(2, p.Stocks);
 
-            // sortir plus que le stock -> rupture partielle
             sorti = p.Sortir(5);
             Assert.AreEqual(2, sorti);
             Assert.AreEqual(0, p.Stocks);
             Assert.IsTrue(p.EstEnRupture);
         }
-
 
     }
 }
